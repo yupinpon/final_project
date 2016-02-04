2aa1b644fee425e217e3a60254bfa4b93e658a54
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-02-03 21:29:14 -0800

    ตกแต่งฉากสองใส่ต้นข้าวโพด

---
+++ CharacterController2D-master/Assets/Demo/script/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public int ballValue;
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore();	
+	}
+
+	void OnTriggerEnter2D(){
+		score += ballValue;
+		UpdateScore();
+
+	}
+	void OnCollisionEnter2D (Collision2D collision){
+		if (collision.gameObject.tag == "a1") {
+			score -= ballValue * 2;
+			UpdateScore();
+		}
+	}
+
+	void UpdateScore () {
+		scoreText.text = "Score: \n" + score;
+
+	}
+}
