bb66df421d3249a4aac82f1bf9e9893700efd091
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-03-29 22:31:20 -0700

    กำหนดขาดหน้าจอ

---
+++ CharacterController2D-master/Assets/WidthCaera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+
+public class WidthCaera : MonoBehaviour {
+
+	public int targetWidthSize = 480;
+	public float pixelsToUnits = 100;
+	void Update () {
+		int height = Mathf.RoundToInt(targetWidthSize / (float)Screen.width * Screen.height);
+		GetComponent<Camera>().orthographicSize = height /pixelsToUnits /2;
+	}
+}
