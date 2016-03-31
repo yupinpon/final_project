3f8c3f34deb7b65f30a522c593307e906c8daf9e
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-03-31 08:14:11 -0700

    เดินได้ไม่เออเร่อ

--- CharacterController2D-master/Assets/Demo/script/Player.cs
+++ CharacterController2D-master/Assets/Demo/script/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour {
 
 	private Rigidbody2D _myRigidbody;
 	private Animator _anim;
+	private bool _facingRight = true;
 	public float speed;
 
 	// Use this for initialization
@@ -17,8 +18,24 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float move = Input.GetAxis("Horizontal");
-		_anim.SetFloat ("Speed", move);
+		_anim.SetFloat ("Speed", Mathf.Abs( move));
 		_myRigidbody.velocity = new Vector2 (move * speed, _myRigidbody.velocity.y);
 	
+		if (_facingRight == true && move < 0)
+		{
+			_facingRight = false;
+			transform.rotation = Quaternion.Euler (transform.rotation.x, 180, transform.rotation.z);
+		}else if (_facingRight ==false && move> 0)
+			{
+				_facingRight = true;
+				transform.rotation = Quaternion.Euler(transform.rotation.x ,0 , transform.rotation.z);
+			}
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				_anim.SetBool("run",true);
+			_myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x,10);
+
+			}
+		}
 	}
-}
+
