1a61c52c82b5dc5349f7428fb7505a6273ab3d77
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-03-31 17:43:03 -0700

    ซีนทดสอบ scen2Test

---
+++ CharacterController2D-master/Assets/Demo/script/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D _myRigidbody;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		_myRigidbody= this.GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Horizontal");
+		_myRigidbody.velocity = new Vector2 (move * speed, _myRigidbody.velocity.y);
+	
+	}
+}
