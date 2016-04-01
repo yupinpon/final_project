5b3c630720e082416fa3c439147dd73092f933ed
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-03-31 18:24:59 -0700

    สิงโตเดินได้

--- CharacterController2D-master/Assets/Demo/script/Player.cs
+++ CharacterController2D-master/Assets/Demo/script/Player.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	private Rigidbody2D _myRigidbody;
+	private Animator _anim;
 	public float speed;
 
 	// Use this for initialization
 	void Start () {
 		_myRigidbody= this.GetComponent<Rigidbody2D>();
+		_anim = GetComponent<Animator> ();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float move = Input.GetAxis("Horizontal");
+		_anim.SetFloat ("Speed", move);
 		_myRigidbody.velocity = new Vector2 (move * speed, _myRigidbody.velocity.y);
 	
 	}
