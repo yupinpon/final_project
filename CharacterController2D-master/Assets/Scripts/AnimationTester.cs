0ec1f384a21fe5804e253f19bfa690883f0bb2d3
Pead Nong <anuchit.prasertsang@gmail.com>
2016-05-05 09:11:04 +0700

    comment code not use

--- CharacterController2D-master/Assets/Scripts/AnimationTester.cs
+++ CharacterController2D-master/Assets/Scripts/AnimationTester.cs
@@ -1,39 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
-
-
-
-public class AnimationTester : MonoBehaviour
-{
-	Animator _animator;
-	CharacterController2D _controller;
-
-
-	void Awake()
-	{
-		_animator = GetComponent<Animator>();
-		_controller = GetComponent<CharacterController2D>();
-	}
-
-
-	void Update()
-	{
-		if( _controller != null )
-		{
-			if( _controller.isGrounded && ( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.RightArrow ) ) )
-			{
-				_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.walk" ) );
-			}
-			else if( _controller.isGrounded )
-			{
-				_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.Idle" ) );
-			}
-		}
-
-		if( Input.GetKeyDown( KeyCode.UpArrow ) )
-			_animator.Play( Animator.StringToHash( "Base Layer.Jump" ) );
-		if( Input.GetKeyDown( KeyCode.K ) )
-			_animator.Play( Animator.StringToHash( "Base Layer.Fall" ) );
-	}
-
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//
+//
+//public class AnimationTester : MonoBehaviour
+//{
+//	Animator _animator;
+//	CharacterController2D _controller;
+//
+//
+//	void Awake()
+//	{
+//		_animator = GetComponent<Animator>();
+//		_controller = GetComponent<CharacterController2D>();
+//	}
+//
+//
+//	void Update()
+//	{
+//		if( _controller != null )
+//		{
+//			if( _controller.isGrounded && ( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.RightArrow ) ) )
+//			{
+//				_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.walk" ) );
+//			}
+//			else if( _controller.isGrounded )
+//			{
+//				_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.Idle" ) );
+//			}
+//		}
+//
+//		if( Input.GetKeyDown( KeyCode.UpArrow ) )
+//			_animator.Play( Animator.StringToHash( "Base Layer.Jump" ) );
+//		if( Input.GetKeyDown( KeyCode.K ) )
+//			_animator.Play( Animator.StringToHash( "Base Layer.Fall" ) );
+//	}
+//
+//}
