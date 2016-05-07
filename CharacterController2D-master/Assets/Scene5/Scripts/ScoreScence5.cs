aea3b2a86db47098395838a01af9c2528b210024
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-05-06 23:19:56 -0700

    add Menu to PS

---
+++ CharacterController2D-master/Assets/Scene5/Scripts/ScoreScence5.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreScence5 : MonoBehaviour {
+
+	public int ballValue;
+	public Text scoreText;
+	public Text Wintext;
+	public int winScore;
+	public string scene;
+
+	private int score;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore();	
+		Wintext.text = "" ;
+
+		count = 0;
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.tag == "meat") {
+			score += ballValue;
+			UpdateScore ();	
+			//	Destroy (other.gameObject);
+		}
+		if (other.tag == "grass") {
+			score -= ballValue;
+			UpdateScore ();	
+			//	Destroy (other.gameObject);
+		}
+
+	}
+
+
+		void UpdateScore () {
+			StartCoroutine ("TimeScrip");
+			scoreText.text = "Score "+winScore+" : " + score;
+
+		}
+
+}
