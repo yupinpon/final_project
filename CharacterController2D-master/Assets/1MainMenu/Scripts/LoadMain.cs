e69dfe6cd1e05a4f06112518edae49ce78e9cd5f
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-05-17 05:48:54 -0700

    Scene Main link Button

---
+++ CharacterController2D-master/Assets/1MainMenu/Scripts/LoadMain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMain : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeScene()
+
+	{	
+		Application.LoadLevel("ChooseAnimal");
+	}
+}
