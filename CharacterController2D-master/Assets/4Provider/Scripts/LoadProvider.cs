8820999c46c32487b3d91186252a758727fa733b
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-05-18 11:37:22 -0700

    Provider ลิ้งไปกลับได้

---
+++ CharacterController2D-master/Assets/4Provider/Scripts/LoadProvider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadProvider : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeScene()
+
+	{	
+		Application.LoadLevel("ChooseAnimal");
+	}
+}
+
+
