427346368efb3975fda6817c4ccc0bc439dd7c4d
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-06-03 11:47:53 -0700

    changeScene goto SceneChootAnimal Finish V.1

---
+++ CharacterController2D-master/Assets/2Choose/Scripts/LoadScene/LoadScene_Elephant.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene_Elephant : MonoBehaviour {
+
+
+	// Use this for initialization
+	public void ChangeScene()
+
+	{	
+		Application.LoadLevel("Scene1_Elephant");
+	}
+}
---
+++ CharacterController2D-master/Assets/2Choose/Scripts/LoadScene/LoadScene_Lion.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene_Lion : MonoBehaviour {
+
+
+	// Use this for initialization
+	public void ChangeScene()
+
+	{	
+		Application.LoadLevel("Scene1_Lion");
+	}
+}
