e9783649f9d08d301ff9e32954dd559f315c3dee
yupinpon <yupinpon.ph.55@ubu.ac.th>
2016-06-10 20:30:29 -0700

    แก้ใขฉาก1-4ของสัตว์ทุกตัว

---
+++ CharacterController2D-master/Assets/Common/Scripts/ButtonControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonControl : MonoBehaviour {
+
+	public Animator Character;
+	public GameObject Charac;
+
+
+
+	public void Left(){
+
+		Character.Play ("Run");
+		Character.transform.rotation = Quaternion.Euler (transform.rotation.x, 180, transform.rotation.z);
+
+	}
+	public void Right(){
+
+		Character.Play ("Run");
+
+
+	}
+
+
+
+
+
+}
